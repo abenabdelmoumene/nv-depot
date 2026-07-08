c9cece183f51e7efddfc4fcad84feb3eb09b738a
Antoine D. <antoine@utopios.net>
2026-07-08 12:10:54 +0200

    Ajout des notions jusqu'au variables (Python)

---
+++ 02-Python/.demos/01-variables.py
@@ -0,0 +1,18 @@
+# Création de variables de types différents
+mon_texte = "Je suis du texte"
+mon_nombre = 123456
+mon_nombre_virgule = 4.123
+mon_booleen = True
+
+# Affichage d'une chaine de caractère directement
+print("Je suis du texte")
+
+# Affichage du contenu des variables créées précédemment
+print(mon_texte)
+print(mon_nombre)
+print(mon_booleen)
+print(mon_nombre_virgule)
+
+# Récupération d'une valeur utilisateur via utilisation de print()
+prenom = input("Veuillez entrer votre prénom: ")
+print(prenom)
