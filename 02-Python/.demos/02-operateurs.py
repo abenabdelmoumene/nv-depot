9edc6b850f2afe5e7398a3ce08fa06b5235d9feb
Antoine D. <antoine@utopios.net>
2026-07-08 12:29:56 +0200

    Ajout des notions d'opérateurs artithmétiques

---
+++ 02-Python/.demos/02-operateurs.py
@@ -0,0 +1,23 @@
+nombre_un = 5
+nombre_deux = 10
+nombre_trois = 4
+
+## Les Opérateurs Arithmétiques
+la_somme = 5 + 10 # 15
+la_somme_variables = nombre_un + nombre_deux
+
+la_concatenation = "5" + "10" # "510"
+
+la_difference = nombre_un - nombre_deux # -5
+
+le_quotient = nombre_deux / nombre_trois # 2.5
+
+le_quotient_euclidien = nombre_deux // nombre_trois # 2
+
+le_produit = nombre_un * nombre_trois # 20
+
+la_multiplication_texte = "ABC" * 3 # "ABCABCABC" 
+
+# La caractère espace, pour un ordinateur => \0
+
+la_puissance = nombre_deux ** nombre_un # 100.000
